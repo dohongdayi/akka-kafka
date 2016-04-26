2583105dda2463733f4c617de7686063c588a1a8
slamhan <slamhan1987@hotmail.com>
2016-04-26 15:27:29 +0800

    refactor refer to https://apache.googlesource.com/kafka/+/0.9.0/core/src/main/scala/kafka/tools/ConsumerOffsetChecker.scala

--- service/src/main/scala/io/github/junheng/akka/kafka/KConsumerManager.scala
+++ service/src/main/scala/io/github/junheng/akka/kafka/KConsumerManager.scala
@@ -8,7 +8,6 @@ import kafka.common.{BrokerNotAvailableException, ErrorMapping, OffsetMetadataAn
 import kafka.consumer.SimpleConsumer
 import kafka.network.BlockingChannel
 import kafka.utils.{Json, ZKGroupTopicDirs, ZkUtils}
-import org.I0Itec.zkclient.ZkClient
 import org.I0Itec.zkclient.exception.ZkNoNodeException
 import org.apache.kafka.common.security.JaasUtils
 
@@ -19,11 +18,13 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
   private val consumerMap: mutable.Map[Int, Option[SimpleConsumer]] = mutable.Map()
   private val offsetMap: mutable.Map[TopicAndPartition, Long] = mutable.Map()
   private var topicPidMap: immutable.Map[String, Seq[Int]] = immutable.Map()
-  private var zkUtils: ZkUtils = null
+  private val zkUtils: ZkUtils = ZkUtils(zookeeper,
+    30000,
+    30000,
+    JaasUtils.isZkSecurityEnabled())
 
-  private def getConsumer(zkClient: ZkClient, bid: Int): Option[SimpleConsumer] = {
+  private def getConsumer(bid: Int): Option[SimpleConsumer] = {
     try {
-      zkUtils = ZkUtils(zkClient, JaasUtils.isZkSecurityEnabled())
       zkUtils.readDataMaybeNull(ZkUtils.BrokerIdsPath + "/" + bid)._1 match {
         case Some(brokerInfoString) =>
           Json.parseFull(brokerInfoString) match {
@@ -40,34 +41,39 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
       }
     } catch {
       case t: Throwable =>
-        log.info("Could not parse broker info due to " + t.getCause)
+        println("Could not parse broker info due to " + t.getCause)
         None
     }
   }
 
-  private def processPartition(zkClient: ZkClient, group: String, topic: String, pid: Int): Option[PartitionStatus] = {
+  private def processPartition(group: String, topic: String, pid: Int): Option[PartitionStatus] = {
     val topicPartition = TopicAndPartition(topic, pid)
     val offsetOpt = offsetMap.get(topicPartition)
     val groupDirs = new ZKGroupTopicDirs(group, topic)
-    zkUtils = ZkUtils(zkClient, JaasUtils.isZkSecurityEnabled())
-    val ownerOpt = zkUtils.readDataMaybeNull(groupDirs.consumerOwnerDir + "/%s".format(pid))._1
+    val owner = zkUtils.readDataMaybeNull(groupDirs.consumerOwnerDir + "/%s".format(pid))._1
     zkUtils.getLeaderForPartition(topic, pid) match {
       case Some(bid) =>
-        val consumerOpt = consumerMap.getOrElseUpdate(bid, getConsumer(zkClient, bid))
-        consumerOpt map { consumer =>
-          val topicAndPartition = TopicAndPartition(topic, pid)
-          val request = OffsetRequest(immutable.Map(topicAndPartition -> PartitionOffsetRequestInfo(OffsetRequest.LatestTime, 1)))
-          val logSize = consumer.getOffsetsBefore(request).partitionErrorAndOffsets(topicAndPartition).offsets.head
-          val lagOpt = offsetOpt.map(o => if (o == -1) -1 else logSize - o)
-          PartitionStatus(group, topic, pid, offsetOpt.getOrElse(-1), logSize, lagOpt.getOrElse(-1), ownerOpt.getOrElse("none"))
+        val consumerOpt = consumerMap.getOrElseUpdate(bid, getConsumer(bid))
+        consumerOpt match {
+          case Some(consumer) =>
+            val topicAndPartition = TopicAndPartition(topic, pid)
+            val request =
+              OffsetRequest(immutable.Map(topicAndPartition -> PartitionOffsetRequestInfo(OffsetRequest.LatestTime, 1)))
+            val logSize = consumer.getOffsetsBefore(request).partitionErrorAndOffsets(topicAndPartition).offsets.head
+            val lagOpt = offsetOpt.map(o => if (o == -1) -1 else logSize - o)
+            Some(PartitionStatus(group, topic, pid, offsetOpt.getOrElse(-1), logSize, lagOpt.getOrElse(-1), owner.getOrElse("none")))
+          case None => None
         }
-      case None => None
+      case None => {
+        println("No broker for partition %s - %s".format(topic, pid))
+        None
+      }
     }
   }
 
-  private def processTopic(zkClient: ZkClient, group: String, topic: String): Option[List[PartitionStatus]] = {
+  private def processTopic(group: String, topic: String): Option[List[PartitionStatus]] = {
     topicPidMap.get(topic) map { pids =>
-      pids.sorted.map(pid => processPartition(zkClient, group, topic, pid)).filterNot(_.isEmpty).map(_.get).toList
+      pids.sorted.map(pid => processPartition(group, topic, pid)).filterNot(_.isEmpty).map(_.get).toList
     }
   }
 
@@ -104,7 +110,7 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
       }
       channel.disconnect()
 
-      val result = topicList.sorted.map(topic => processTopic(zkUtils.zkClient, group, topic)).filterNot(_.isEmpty).map(_.get).foldLeft(List[PartitionStatus]())(_ ::: _)
+      val result = topicList.sorted.map(topic => processTopic(group, topic)).filterNot(_.isEmpty).map(_.get).foldLeft(List[PartitionStatus]())(_ ::: _)
 
       for ((_, consumerOpt) <- consumerMap)
         consumerOpt match {
