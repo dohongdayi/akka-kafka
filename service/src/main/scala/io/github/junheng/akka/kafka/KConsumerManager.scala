98624ea712d503d1124e655d412bf2329d1ff150
slamhan <slamhan1987@hotmail.com>
2016-04-26 15:02:59 +0800

    Migration for kakfa 0.9.0.1

--- service/src/main/scala/io/github/junheng/akka/kafka/KConsumerManager.scala
+++ service/src/main/scala/io/github/junheng/akka/kafka/KConsumerManager.scala
@@ -7,9 +7,10 @@ import kafka.client.ClientUtils
 import kafka.common.{BrokerNotAvailableException, ErrorMapping, OffsetMetadataAndError, TopicAndPartition}
 import kafka.consumer.SimpleConsumer
 import kafka.network.BlockingChannel
-import kafka.utils.{Json, ZKGroupTopicDirs, ZKStringSerializer, ZkUtils}
+import kafka.utils.{Json, ZKGroupTopicDirs, ZkUtils}
 import org.I0Itec.zkclient.ZkClient
 import org.I0Itec.zkclient.exception.ZkNoNodeException
+import org.apache.kafka.common.security.JaasUtils
 
 import scala.collection.{Map, Seq, immutable, mutable}
 import scala.concurrent.{ExecutionContext, Future}
@@ -18,10 +19,12 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
   private val consumerMap: mutable.Map[Int, Option[SimpleConsumer]] = mutable.Map()
   private val offsetMap: mutable.Map[TopicAndPartition, Long] = mutable.Map()
   private var topicPidMap: immutable.Map[String, Seq[Int]] = immutable.Map()
+  private var zkUtils: ZkUtils = null
 
   private def getConsumer(zkClient: ZkClient, bid: Int): Option[SimpleConsumer] = {
     try {
-      ZkUtils.readDataMaybeNull(zkClient, ZkUtils.BrokerIdsPath + "/" + bid)._1 match {
+      zkUtils = ZkUtils(zkClient, JaasUtils.isZkSecurityEnabled())
+      zkUtils.readDataMaybeNull(ZkUtils.BrokerIdsPath + "/" + bid)._1 match {
         case Some(brokerInfoString) =>
           Json.parseFull(brokerInfoString) match {
             case Some(m) =>
@@ -46,8 +49,9 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
     val topicPartition = TopicAndPartition(topic, pid)
     val offsetOpt = offsetMap.get(topicPartition)
     val groupDirs = new ZKGroupTopicDirs(group, topic)
-    val ownerOpt = ZkUtils.readDataMaybeNull(zkClient, groupDirs.consumerOwnerDir + "/%s".format(pid))._1
-    ZkUtils.getLeaderForPartition(zkClient, topic, pid) match {
+    zkUtils = ZkUtils(zkClient, JaasUtils.isZkSecurityEnabled())
+    val ownerOpt = zkUtils.readDataMaybeNull(groupDirs.consumerOwnerDir + "/%s".format(pid))._1
+    zkUtils.getLeaderForPartition(topic, pid) match {
       case Some(bid) =>
         val consumerOpt = consumerMap.getOrElseUpdate(bid, getConsumer(zkClient, bid))
         consumerOpt map { consumer =>
@@ -68,19 +72,15 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
   }
 
   def getPartitionStatus: Future[List[PartitionStatus]] = Future {
-    var zkClient: ZkClient = null
     val channel: BlockingChannel = null
     try {
-      zkClient = new ZkClient(zookeeper, 30000, 30000, ZKStringSerializer)
-
       val topicList = List(topic)
-
-      topicPidMap = immutable.Map(ZkUtils.getPartitionsForTopics(zkClient, topicList).toSeq: _*)
+      topicPidMap = immutable.Map(zkUtils.getPartitionsForTopics(topicList).toSeq: _*)
       val topicPartitions = topicPidMap.flatMap { case (_topic, partitionSeq) => partitionSeq.map(TopicAndPartition(_topic, _)) }.toSeq
-      val channel = ClientUtils.channelToOffsetManager(group, zkClient)
+      val channel = ClientUtils.channelToOffsetManager(group, zkUtils)
 
       channel.send(OffsetFetchRequest(group, topicPartitions))
-      val offsetFetchResponse = OffsetFetchResponse.readFrom(channel.receive().buffer)
+      val offsetFetchResponse = OffsetFetchResponse.readFrom(channel.receive().payload)
 
       offsetFetchResponse.requestInfo.foreach { case (topicAndPartition, offsetAndMetadata) =>
         if (offsetAndMetadata == OffsetMetadataAndError.NoOffset) {
@@ -88,10 +88,10 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
           // this group may not have migrated off zookeeper for offsets storage (we don't expose the dual-commit option in this tool
           // (meaning the lag may be off until all the consumers in the group have the same setting for offsets storage)
           try {
-            val offset = ZkUtils.readData(zkClient, topicDirs.consumerOffsetDir + "/%d".format(topicAndPartition.partition))._1.toLong
+            val offset = zkUtils.readData(topicDirs.consumerOffsetDir + "/%d".format(topicAndPartition.partition))._1.toLong
             offsetMap.put(topicAndPartition, offset)
           } catch {
-            case zkNoNodeException: ZkNoNodeException if ZkUtils.pathExists(zkClient, topicDirs.consumerOffsetDir) =>
+            case zkNoNodeException: ZkNoNodeException if zkUtils.pathExists(topicDirs.consumerOffsetDir) =>
               offsetMap.put(topicAndPartition, -1)
             case ex: Exception => throw ex
           }
@@ -104,7 +104,7 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
       }
       channel.disconnect()
 
-      val result = topicList.sorted.map(topic => processTopic(zkClient, group, topic)).filterNot(_.isEmpty).map(_.get).foldLeft(List[PartitionStatus]())(_ ::: _)
+      val result = topicList.sorted.map(topic => processTopic(zkUtils.zkClient, group, topic)).filterNot(_.isEmpty).map(_.get).foldLeft(List[PartitionStatus]())(_ ::: _)
 
       for ((_, consumerOpt) <- consumerMap)
         consumerOpt match {
@@ -118,29 +118,23 @@ class KConsumerManager(topic: String, group: String, zookeeper: String, log: Log
         case Some(consumer) => consumer.close()
         case None =>
       }
-      if (zkClient != null) zkClient.close()
+      if (zkUtils.zkClient != null) zkUtils.zkClient.close()
       if (channel != null) channel.disconnect()
     }
   }
 
   def rewind(partitions: Int): Future[Unit] = Future {
     val partitionOffsets = 1 to partitions map (i => s"/consumers/$group/offsets/$topic/$i") map (k => k -> "0") toMap
-    var zkClient: ZkClient = null
-    try {
-      zkClient = new ZkClient(zookeeper, 30000, 30000, ZKStringSerializer)
 
-      for ((partition, offset) <- partitionOffsets) {
-        log.info("updating [" + partition + "] with offset [" + offset + "]")
+    for ((partition, offset) <- partitionOffsets) {
+      log.info("updating [" + partition + "] with offset [" + offset + "]")
 
-        try {
-          ZkUtils.updatePersistentPath(zkClient, partition, offset.toString)
-        } catch {
-          case e: Throwable => e.printStackTrace()
-        }
+      try {
+        zkUtils.updatePersistentPath(partition, offset.toString)
+      } catch {
+        case e: Throwable => e.printStackTrace()
       }
-    } finally {
-      if (zkClient != null) zkClient.close()
     }
-
+    if (zkUtils.zkClient != null) zkUtils.zkClient.close()
   }
 }
